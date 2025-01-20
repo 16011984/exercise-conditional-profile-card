ea42e12592eccd9cd0a589f9fe456dfa5d5bee55
Dani Yanani <alejandrorot2@gmail.com>
2025-01-20 20:23:38 +0000

    presentacion_2

--- src/js/app.js
+++ src/js/app.js
@@ -43,10 +43,22 @@ function render(variables = {}) {
           <h3>${variables.city || `City`} ${variables.country || `Country`}</h3>
 
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+
+            <li><a href="https://github.com/${
+              variables.github
+            }y"><i class="fab fa-github"></i></a></li>
+
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
+            
           </ul>
         </div>
     `;
